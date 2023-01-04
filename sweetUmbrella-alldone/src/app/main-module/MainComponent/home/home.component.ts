4023c696ed5d7ce7d8c27f3813535f883939a6e6
fazi1live <fazi1live@gmail.com>
2023-01-04 07:23:34 +0500

    add

--- sweetUmbrella-alldone/src/app/main-module/MainComponent/home/home.component.ts
+++ sweetUmbrella-alldone/src/app/main-module/MainComponent/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public formModel: FormGroup | any;
 
-  ngOnInit(): void {
+  constructor(
+    private readonly FormBuilder:FormBuilder
+  ) { 
+    this.formInitialization();
   }
 
-  // scroll(el:HTMLElement){
-  //   el.scrollIntoView();
-  // }
+  ngOnInit(): void {
+
+  }
+
+  public formInitialization(){
+    this.formModel = this.FormBuilder.group({
+      firstName:new FormControl('',[Validators.required, Validators.pattern(/^[a-zA-Z ]*$/)]),
+      lastName:new FormControl('',[Validators.required, Validators.pattern(/^[a-zA-Z ]*$/)]),
+      emailAddress:new FormControl('',[Validators.required, Validators.email]),
+      subject:new FormControl('',[Validators.required,Validators.maxLength(20)]),
+      message:new FormControl('',[Validators.required,Validators.maxLength(500)])
+    })
+  }
+
+  public submitForm(){
+    const formValues = this.formModel.values;
+  }
+
 }
