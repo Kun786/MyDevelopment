0b52117a7358fd87736272c421acfd6baa2f05da
fazi1live <fazi1live@gmail.com>
2023-01-10 16:11:05 +0500

    add

--- sweetUmbrella-alldone/src/app/shared/services/client-form.service.ts
+++ sweetUmbrella-alldone/src/app/shared/services/client-form.service.ts
@@ -1,19 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientFormService {
-  private baseUrl: string | any = environment.baseUrl;
   constructor(
     private readonly HttpClient:HttpClient
   ) { }
 
   submitClientQuery(payLoad:any){
-    console.log(payLoad)
-    console.log(this.baseUrl)
-    return this.HttpClient.post(`${this.baseUrl}/emailManagement/sendEmail`,payLoad);
+    return this.HttpClient.post('/emailManagement/sendEmail',payLoad);
   }
 }
